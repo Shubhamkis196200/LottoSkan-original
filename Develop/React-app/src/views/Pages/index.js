986520c16c4b95115b8cfe4d2e006a36df504629
Shubhamkis196200 <160484828+Shubhamkis196200@users.noreply.github.com>
2025-02-15 11:47:45 +0100

    Delete Develop/React-app/src directory

--- Develop/React-app/src/views/Pages/index.js
+++
@@ -1,7 +0,0 @@
-import Login from "./Login";
-import Page404 from "./Page404";
-import Page500 from "./Page500";
-import Register from "./Register";
-import ForgotPassword from "./ForgotPassword/ForgotPassword";
-
-export { Login, Page404, Page500, Register, ForgotPassword };
